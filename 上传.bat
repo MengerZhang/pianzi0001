9f84e3220c12c889de369c7365b709c3dd2dbdae
MengerZhang <menger_hacker@163.com>
2018-02-22 00:44:38 +0800

    ok
    ok

---
+++ 上传.bat
@@ -0,0 +1,8 @@
+echo "# pianzi0001.io" >> README.md
+git init
+git add README.md
+git add.
+git commit -m "first commit"
+git remote add origin git@github.com:MengerZhang/pianzi0001.io.git
+git push -u origin master
+pause
